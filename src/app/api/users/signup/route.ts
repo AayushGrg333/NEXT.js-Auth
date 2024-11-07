c668c9429094f348cba1a9d639d0d80ca7f9279d
AayushGrg333 <aayushgrg333@gmail.com>
2024-11-07 22:00:58 +0545

    added

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -41,7 +41,7 @@ export async function POST(request : NextRequest) {
             return NextResponse.json({
                 message:"user registered successfuly",
                 success: true,
-                savedUser,
+                savedUser
             })
             
 
